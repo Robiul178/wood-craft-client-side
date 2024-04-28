4a0c305a904f82290ea41874d6f9b28409551dc0
Robiul Islam <robiulislam.me2022@gmail.com>
2024-04-28 15:17:32 +0600

    add footer

--- src/components/Root/Root.jsx
+++ src/components/Root/Root.jsx
@@ -1,6 +1,6 @@
 import { Outlet } from "react-router-dom";
 import NaveBar from "../Pages/NaveBar/NaveBar";
-// import Footer from "../Pages/Footer/Footer";
+import Footer from "../Pages/Footer/Footer";
 
 
 const Root = () => {
@@ -8,7 +8,7 @@ const Root = () => {
         <div className="max-w-[1200px] mx-auto">
             <NaveBar />
             <Outlet />
-            {/* <Footer /> */}
+            <Footer />
         </div>
     );
 };
