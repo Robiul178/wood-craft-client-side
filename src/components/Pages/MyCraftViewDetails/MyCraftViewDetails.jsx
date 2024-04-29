9a00b42967882d01252b0c8aa86f9709a93f1c91
Robiul Islam <robiulislam.me2022@gmail.com>
2024-04-30 02:28:24 +0600

    add updat delete button

--- src/components/Pages/MyCraftViewDetails/MyCraftViewDetails.jsx
+++ src/components/Pages/MyCraftViewDetails/MyCraftViewDetails.jsx
@@ -1,18 +1,45 @@
 
-import { Link } from 'react-router-dom';
 
 const MyCraftViewDetails = ({ item }) => {
-    const { _id, photo, name, description, price } = item;
+    const { photo, name, customization, stockStatus, rating, price } = item;
+
+    const handleDeleteButton = () => {
+        console.log('deleted')
+    }
+    const handleUpdateButton = () => {
+        console.log('updated')
+    }
+
 
     return (
         <div >
             <div className="card lg:card-side bg-base-100 shadow-xl">
-                <figure><img src="https://daisyui.com/images/stock/photo-1494232410401-ad00d5433cfa.jpg" alt="Album" /></figure>
+                <figure><img src={photo} alt="Album" /></figure>
                 <div className="card-body">
-                    <h2 className="card-title">New album is released!</h2>
-                    <p>Click the button to listen on Spotiwhy app.</p>
-                    <div className="card-actions justify-end">
-                        <button className="btn btn-primary">Listen</button>
+                    <h2 className="card-title">{name}</h2>
+                    <p className="text-xl font-bold font-mono">Price: ${price}</p>
+
+                    <div className="flex py-0">
+                        <span>
+                            <h2 className="text-lg mb-2"><span className="font-semibold">{rating}</span></h2>
+                        </span>
+                        <span>
+                            <div className="rating">
+                                <input type="radio" name="rating-2" className="mask mask-star-2 bg-orange-400" />
+                                <input type="radio" name="rating-2" className="mask mask-star-2 bg-orange-400" />
+                                <input type="radio" name="rating-2" className="mask mask-star-2 bg-orange-400" />
+                                <input type="radio" name="rating-2" className="mask mask-star-2 bg-orange-400" checked />
+                                <input type="radio" name="rating-2" className="mask mask-star-2 bg-orange-400" />
+                            </div>
+                        </span>
+                    </div>
+
+                    <p className="py-0">Customization: {customization}</p>
+                    <p className="text-green-600"> {stockStatus}</p>
+
+                    <div className="card-actions justify-between">
+                        <button onClick={handleUpdateButton} className="btn btn-primary">Update</button>
+                        <button onClick={handleDeleteButton} className="btn btn-outline">Delete</button>
                     </div>
                 </div>
             </div>
