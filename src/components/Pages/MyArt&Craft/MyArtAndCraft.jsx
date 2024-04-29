1d62102c337f60678ebfd584800dfea2f27b2830
Robiul Islam <robiulislam.me2022@gmail.com>
2024-04-30 03:44:21 +0600

    add delete functionality

--- src/components/Pages/MyArt&Craft/MyArtAndCraft.jsx
+++ src/components/Pages/MyArt&Craft/MyArtAndCraft.jsx
@@ -18,10 +18,26 @@ const MyArtAndCraft = () => {
     }, [user])
 
 
+
+    const onClickYes = c => {
+        console.log(myItem, c)
+        const myItems = myItem?.find(i => i.customization == c);
+        setMyItem(myItems)
+    }
+
     return (
         <div>
             <div>
                 <h2 className="text-3xl">My Art & Craft {myItem?.length}</h2>
+            </div>
+            <div>
+                <div className="dropdown dropdown-end">
+                    <div tabIndex={0} role="button" className="btn m-1">Filter</div>
+                    <ul tabIndex={0} className="dropdown-content z-[1] menu p-2 shadow bg-base-100 rounded-box w-52">
+                        <li><a onClick={() => onClickYes('Yes')}>Yes</a></li>
+                        {/* <li><a onClick={() => onClickNo()}>No</a></li> */}
+                    </ul>
+                </div>
             </div>
             <div className='grid md:grid-cols-2 sm:p-10 mt-12 gap-4 '>
                 {
